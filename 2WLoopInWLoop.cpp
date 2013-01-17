0c0d6db6c3b44a98bb2588fb464180185de48bdb
JamPol <james.polson@live.ca>
2013-01-17 08:54:39 -0700

    Complete
    While loop within a while loop.

---
+++ 2WLoopInWLoop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    
+    while ( x < 10 ) {
+          while ( x < 5 ) {
+              cout<< x * 3 <<endl;
+              x++;
+          }
+          cout<< x <<endl;
+          x++;
+    }
+    cin.get();
+}
